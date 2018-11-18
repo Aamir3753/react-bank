22c652a2b69a18e58a7eb771c8930f4757033635
Aamir Jutt <ars3753669@gmail.com>
2018-11-18 22:01:55 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React , {Component} from 'react';
-class App extends Component{
-    render(){
-        return(
-            <h1>Hello World</h1>
+import React, { Component } from 'react';
+class App extends Component {
+    render() {
+        return (
+            <div >Hello</div>
         )
     }
 }
