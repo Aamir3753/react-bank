5c98ef5bc73d20e58b6c7cce732eeb51dae31b59
Aamir Jutt <ars3753669@gmail.com>
2018-11-21 12:21:27 +0500

    Updates

---
+++ precache-manifest.d6da94fd049013c905e61e22120534d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a185801139bd550902c",
+    "url": "/react-bank/static/js/runtime~main.5a185801.js"
+  },
+  {
+    "revision": "f5995024d71a45680b38",
+    "url": "/react-bank/static/js/main.f5995024.chunk.js"
+  },
+  {
+    "revision": "8a15db931dc37c1e966c",
+    "url": "/react-bank/static/js/1.8a15db93.chunk.js"
+  },
+  {
+    "revision": "8a15db931dc37c1e966c",
+    "url": "/react-bank/static/css/1.76deb9c5.chunk.css"
+  },
+  {
+    "revision": "f0bd41292a330f4b0f255ccabc477ba1",
+    "url": "/react-bank/index.html"
+  }
+];
